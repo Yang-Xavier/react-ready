040110165d3a925c1a07729004de724ba99d2bd3
mpsss <mephistommm@gmail.com>
2016-01-03 11:16:51 +0800

    change some things

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
 import React, { Component  } from 'react'
 import AppBar from 'material-ui/lib/app-bar'
 import FontIcon from 'material-ui/lib/font-icon'
+import FlatButton from 'material-ui/lib/flat-button'
 import ThemeManager from 'material-ui/lib/styles/theme-manager'
+import IconMenu from 'material-ui/lib/menus/icon-menu'
+import MenuItem from 'material-ui/lib/menus/menu-item'
+import MenuDivider from 'material-ui/lib/menus/menu-divider'
+import IconButton from 'material-ui/lib/icon-button'
 import MyTheme from './selfTheme.js'
+import Colors from 'material-ui/lib/styles/colors'
+
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
+
+class HeadMenuListButton extends Component {
+  
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <IconMenu iconButtonElement={
+        <IconButton tooltip={'menus list'} >
+          <FontIcon className='material-icons' color={Colors.fullWhite} >more_vert</FontIcon>
+        </IconButton> } >
+        <MenuItem primaryText="Refresh" />
+        <MenuItem primaryText="Send feedback" />
+        <MenuItem primaryText="Settings" />
+        <MenuDivider />
+        <MenuItem primaryText="Help" />
+        <MenuItem primaryText="Sign out" />
+      </IconMenu>
+    )
+  }
+}
 
 export default class App extends Component {
 
@@ -23,15 +56,15 @@ export default class App extends Component {
   }
 
   render() {
-    var like = this.state.liked ? "like" : "haven't liked"
+    var like = this.state.liked ? 'like' : 'haven\'t liked'
 
     return (
-      <div className="App" >
-        <AppBar title="Expriment" 
-                iconElementRight={<FontIcon className="material-icons" style={{color: "#ffffff"}}>flight_takeoff</FontIcon>} 
+      <div className='App' >
+        <AppBar title='Expriment' 
+                iconElementRight={<HeadMenuListButton />} 
         />
         <p>I {like} this button! </p>
-        <input type="button" value="click me" onClick={this.handleClick.bind(this)} />
+        <input type='button' value='click me' onClick={this.handleClick.bind(this)} />
       </div>
     );
   }
--- src/entry.jsx
+++ src/entry.jsx
@@ -1,10 +1,36 @@
-import React from 'react';
-import App from './App.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import IconMenu from 'material-ui/lib/menus/icon-menu'
+import MenuItem from 'material-ui/lib/menus/menu-item'
+import IconButton from 'material-ui/lib/icon-button'
+import FontIcon from 'material-ui/lib/font-icon'
 
-let ReactDOM = require('react-dom');
+import App from './App.jsx'
+
+injectTapEventPlugin()
+
+var Main = React.createClass({
+  render: function() {
+    return (
+      <IconMenu iconButtonElement={
+        <IconButton tooltip={'menus list'} >
+          <FontIcon className='material-icons' style={{color: '#ffffff'}} >more_vert</FontIcon>
+        </IconButton> } >
+        <MenuItem primaryText="Refresh" />
+        <MenuItem primaryText="Send feedback" />
+        <MenuItem primaryText="Settings" />
+        <MenuItem primaryText="Help" />
+        <MenuItem primaryText="Sign out" />
+      </IconMenu>
+    );
+  }
+
+});
 
 // a glue fucntion to glue django template and React.
 window.ReactInit = function glue() {
-  ReactDOM.render(<App style={{width: 100}}/>, document.getElementById('root'));
+  ReactDOM.render(<App style={{width: 100}}/>, document.getElementById('root'))
+  //ReactDOM.render(<Main />, document.getElementById("container"));
 }
 
